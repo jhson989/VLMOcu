8c3ea42e1300fbcb672a54603f69d9c628331fe4
Janghyun Son <jhson989@gmail.com>
2021-05-28 16:18:53 +0900

    temp

--- include/kernel.cuh
+++ include/kernel.cuh
@@ -6,8 +6,11 @@
 __global__ void cuda_element_add (const float *A, const float *B, float *C, int length);
 __global__ void cuda_element_add_patch (const float *A, const float *B, float *C, const size_t done, const size_t num_elements);
 __global__ void cuda_element_sub (const float *A, const float *B, float *C, int length);
+__global__ void cuda_element_sub_patch (const float *A, const float *B, float *C, const size_t done, const size_t num_elements);
 __global__ void cuda_element_mul (const float *A, const float *B, float *C, int length);
+__global__ void cuda_element_mul_patch (const float *A, const float *B, float *C, const size_t done, const size_t num_elements);
 __global__ void cuda_element_div (const float *A, const float *B, float *C, int length);
+__global__ void cuda_element_div_patch (const float *A, const float *B, float *C, const size_t done, const size_t num_elements);
 
 /** Matrix multiplication **/
 __global__ void cuda_matrix_mul_basic (const float *A, const float *B, float *C, const size_t M, const size_t N, const size_t K);
